6847fe24094cac0f904f7e27d2d210703545ea4f
Parker Parker <792797@aacps.org>
2020-11-09 15:50:22 +0000

    testPalindromicNum() and reverseNum() should work, searchForPalindromicNum() should work as well

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -1,6 +1,16 @@
 public class PalindromicNumberFinder {
     private int num;
 
+
+    //added variables
+    private String original = "";
+    private String reverse = "";
+    private int reversedInt = 0;
+    private int temp;
+    private int next = 0;
+    private boolean isPalindrome;
+  
+
     // constructor
     public PalindromicNumberFinder(int num){
         this.num = num;
@@ -12,22 +22,52 @@ public class PalindromicNumberFinder {
     }
 
     // this method should find the next palindromic number
+    
+    //test this/fix this cuz this one takes too long to output
     public int searchForPalindromicNum(int num){
-
+         next = num + 1;
+         while(next != num && isPalindrome != true){
+         next = next + 10000;
+         isPalindrome = testPalindromicNum(next);
+         //System.out.println(isPalindrome);
+         }
+        return next - num;
+         
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
-    public boolean testPalindromicNum(int num){
 
+    public boolean testPalindromicNum(int num){
+      reverseNum(num);
+      if(original.equals(reverse) && num == reversedInt){
+        return true;
+      }else{
+        return false;
+      } 
 
     }
 
-
     // this is a helper method for testPalindromicNum. It should reverse the number and return it.
     public String reverseNum(int num){
+      //this for loop is technically uneccesary, if converted to return type int.
+      original = original + num;
+      int length = original.length() - 1;
+      //reverses the number as a string
+      for(int i = length; i >= 0 ; i--){
+        
+        reverse = reverse + original.charAt(i);
+      }
 
+      //reverses the number as an int as well;
+      while (num != 0){
+        temp = num % 10;
+        reversedInt = reversedInt * 10 + temp;
+        num /= 10;
+      }
 
-
+      //test int reversing
+      //System.out.println(reversedInt);
+      return reverse;
     }
 
 
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,10 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-
+        int num = 72927;
+        PalindromicNumberFinder p = new PalindromicNumberFinder(num);
+        System.out.println(p);
+        // boolean isPalindrone = p.testPalindromicNum(num);
+        // System.out.println(isPalindrone);
     }
 }
